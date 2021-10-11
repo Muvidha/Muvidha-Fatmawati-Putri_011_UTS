88292d920e886abd678be8a48da55df153348bf3
Muvidha <90436781+Muvidha@users.noreply.github.com>
2021-10-11 09:57:44 +0700

    Add files via upload

---
+++ Muvidha Fatmawati Putri_011_1.Kategori.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <conio.h>
+using  namespace  std ;
+
+int  utama ()
+{
+
+// Data
+
+    nama string;
+	int nama, umur;
+
+    // Masuk
+
+    cout<< " --------------- " <<endl;
+    cout<< " Kategori Umur Pada Manusia " << endl;
+    cout<< " --------------- " <<endl;
+
+
+    // Masukan Data
+
+
+    cout<< " Masukkan Nama anda = " ;
+    getline (cin,nama);
+
+    Mulailah:
+	cout<< " Masukkan Umur anda = " ;
+	cin>>umur;
+
+
+	// Deklarasi
+
+	if (umur>= 60 && umur<= 100 ){
+		cout<< " \n Anda Termasuk Kategori Lansia " ;
+
+	} else  if (umur>= 30 && umur<= 59 ){
+		cout<< " Anda Termasuk Kategori Dewasa " ;
+
+	} else  if (umur>= 18 && umur<= 29 ){
+		cout<< " Anda Termasuk Kategori Pemuda " ;
+
+	} else  if (umur>= 12 && umur<= 17 ){
+		cout<< " Anda Termasuk Kategori Remaja " ;
+
+	} else  if (umur>= 6 && umur<= 11 ){
+		cout<< " Anda Termasuk Kategori Anak-anak " ;
+
+    } else  if (umur>= 3 && umur<= 5 ){
+		cout<< " Anda Termasuk Kategori Balita " ;
+
+    } else  if (umur>= 1 && umur<= 2 ){
+		cout<< " Anda Termasuk Kategori Batita " ;
+
+    } else  if (umur>= 0 && umur<= 0 ){
+		cout<< " Anda Termasuk Kategori Bayi " ;
+
+
+	}
+	{
+		cout<< " Umur yang anda masukan salah " <<endl;
+		harusmulai;
+	}
+
+    return 0; 
+}
